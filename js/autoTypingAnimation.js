72e14c16d13112fb127409132e2af178fc21cffb
Tanish Srivastava <134135447+tanishS123@users.noreply.github.com>
2023-05-26 21:47:48 +0530

    added index.html

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Tanish Srivastava </span> "Good morning/afternoon! My name is [Tanish Srivastava], and I'm thrilled to have the opportunity to interview for this position. I am a highly motivated and experienced professional with a strong background in technical field. I have many skilled that i have mentioned in my skilled section.I like problem solving,designing the website,video editing.I am very keen interersted to learn new skills.because we know that in the new era we should  have multiple skills
+        Therefore we should try to improve our skills day be day for a succesful future".
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
